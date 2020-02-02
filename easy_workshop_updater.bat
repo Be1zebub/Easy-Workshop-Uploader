b7fcc91805bd3dfb491a3b121f55ca3687c4807b
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-02-03 03:02:28 +0700

    Create easy_workshop_updater.bat

---
+++ easy_workshop_updater.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set gmpublish="Q:\SteamLibrary\steamapps\common\GarrysMod\bin\gmpublish.exe" &REM —> Paste path to your gmpublish.exe here!
+
+set /p workshopid="Enter WorkshopID: "
+set /p changes="Enter Changes: "
+set GMA=%~1
+
+echo Easy Workshop Uploader
+echo -------------------------------------
+echo Path to gmpublish.exe: %gmpublish%
+echo Path to .gma Addon: %GMA%
+echo WorkshopID: %workshopid%
+echo Changes: %changes%
+echo -------------------------------------
+
+
+"%gmpublish%" update -addon "%GMA%" -id "%workshopid%" -changes "%changes%"
